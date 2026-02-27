7046e65da1d41ca5646e16a4e253bea4d577cf5d
fabiovivajardim-ux <fabiovivajardim@gmail.com>
2026-02-27 15:38:03 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,18 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import {defineConfig} from 'vite';
 
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
-  return {
-    base: '/plantas/',
-    plugins: [react(), tailwindcss()],
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+export default defineConfig({
+  base: './', // <--- Isso torna os caminhos relativos e resolve a tela branca
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
     },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
-    server: {
-      hmr: process.env.DISABLE_HMR !== 'true',
-    },
-  };
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 });
