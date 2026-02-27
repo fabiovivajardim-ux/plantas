38521bb10fa1fc0cbe3f01f5b6d47b6f9e9fab4d
fabiovivajardim-ux <fabiovivajardim@gmail.com>
2026-02-27 15:32:55 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/plantas/', // <--- IMPORTANTE: Isso resolve o erro de tela branca
+    base: '/plantas/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
